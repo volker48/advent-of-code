7fe385d87a34895cba7224cae680776ee0797c03
Marcus McCurdy <mmccurdy@obsidiansecurity.com>
2020-12-01 12:48:42 -0500

    day 1 rust

---
+++ 2020/src/day1.rs
@@ -0,0 +1,21 @@
+use itertools::Itertools;
+
+pub fn part1() -> (i32, i32) {
+    let raw_input = include_str!("day1_input.txt");
+    let expenses: Vec<i32> = raw_input.lines().map(|x| x.trim().parse::<i32>().unwrap()).collect();
+    let result = expenses.into_iter()
+    .tuple_combinations()
+    .map(|(x, y)| (x+y, x, y))
+    .find(|(sum, _, _)| *sum == 2020).unwrap();
+    (result.1, result.2)
+}
+
+pub fn part2() -> (i32, i32, i32) {
+    let raw_input = include_str!("day1_input.txt");
+    let expenses: Vec<i32> = raw_input.lines().map(|x| x.trim().parse::<i32>().unwrap()).collect();
+    let result = expenses.into_iter()
+    .tuple_combinations()
+    .map(|(x, y, z)| (x+y+z, x, y, z))
+    .find(|(sum, _, _, _)| *sum == 2020).unwrap();
+    (result.1, result.2, result.3)
+}
--- 2020/src/main.rs
+++ 2020/src/main.rs
@@ -1,3 +1,8 @@
+mod day1;
+
 fn main() {
-    println!("Hello, world!");
+    let (x, y) = day1::part1();
+    println!("part 1 {}", x*y);
+    let (x, y, z) = day1::part2();
+    println!("part 2 {}", x*y*z);
 }
